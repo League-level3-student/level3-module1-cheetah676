267061a4e9e446f576eb0eaf494c5c71eb39083f
cheetah676 <league@WTS20.attlocal.net>
2019-02-10 11:26:41 -0800

    starting dynamicIntegarArray

--- src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
+++ src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
@@ -2,33 +2,36 @@ package _00_Dynamic_Integer_Array;
 
 public class DynamicIntegerArray {
 	//1. Create a private int array. Don't initialize it.
-
+int []array;
 	
 	public DynamicIntegerArray() {
 		//2. Initialize the int array to have 0 elements. 
 		//   This will prevent a null pointer exception
-	
+	array=new int[0];
 	}
 	
 	//3. Complete the steps in the add method
 	public void add(int v) {
 		//A. create and initialize a new int array to be one 
 		//   element longer than the member array
-		
+		int[] addArray=new int[array.length+1];
 		//B. set the last element of your new array to 
 		//   the value passed into the method
-		
+		addArray[addArray.length-1]=v;
 		//C. iterate through the member array and 
 		//   copy every element from the member array 
 		//   to the new array
-		
+		for (int i = 0; i < array.length; i++) {
+			array[i]=addArray[i];
+		}
 		//D. set the member array equal to the new array.
+		array=addArray;
 	}
 	
 	//4. Complete the steps in the get method
 	public int get(int location) {
-		//A. Return the value of the memeber array at the location passed in
-		return 0;
+		//A. Return the value of the member array at the location passed in
+		return array[location];
 	}
 	
 	//5. Run the DynamicArrayTest to see if you are correct so far.
