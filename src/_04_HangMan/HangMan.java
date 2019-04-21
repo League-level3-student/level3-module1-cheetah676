e3861cbacacd39eef2e53c1507293260957c84bc
f*** you <beep@beep.com>
2019-04-21 12:59:34 -0700

    Hangman ip

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,7 +1,18 @@
 package _04_HangMan;
 
+import java.util.Stack;
+
+import javax.swing.JOptionPane;
+
 public class HangMan{
-	
-	
+	Stack <String> allWords=new Stack <String>();
+	public static void main(String[]args) {
+		HangMan HM=new HangMan();
+		HM.run();
+	}
+	void run(){
+		allWords.
+		String words=JOptionPane.showInputDialog(null, "How many word do you want, under 266");
+	}
 	
 }
