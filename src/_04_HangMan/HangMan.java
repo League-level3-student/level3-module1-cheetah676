ec0a7b2ec780755734bea3d13b22c78f4b380cca
League Student <league@iMac-11.attlocal.net>
2019-05-12 11:26:52 -0700

    hangman ip check typed method

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -11,15 +11,19 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class HangMan extends Utilities implements KeyListener {
+	boolean wordComplete = true;
 	Stack<String> allWords = new Stack<String>();
-	Stack singleWord = new Stack<String>();
+	Stack<Character> typedInStack=new Stack<Character>();
 	String underscores = "";
 	String word = "";
-	int numberOfLives=10;
+	int numberOfLives = 10;
+	String stringLives = Integer.toString(numberOfLives);
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
 	JLabel lives = new JLabel();
+	int totalTyped = 0;
+
 	public static void main(String[] args) {
 		HangMan HM = new HangMan();
 		HM.run();
@@ -29,8 +33,9 @@ public class HangMan extends Utilities implements KeyListener {
 		frame.add(panel);
 		panel.add(label);
 		panel.add(lives);
-	numberOfLives.to
-		lives.setText(stringNumberOfLives);
+		frame.setVisible(true);
+		lives.setText("   Lives=" + stringLives);
+		frame.addKeyListener(this);
 		String randomWord;
 		boolean newWord = true;
 		String words = JOptionPane.showInputDialog(null, "How many word do you want, under 266");
@@ -54,38 +59,55 @@ public class HangMan extends Utilities implements KeyListener {
 					}
 				}
 			}
-			for (int i = 0; i < numberOfWords; i++) {
-				String pop = allWords.pop();
-				for (int j = 0; j < pop.length(); j++) {
-					char letter = pop.charAt(j);
-					word += letter;
-					underscores += "_";
-					frame.addKeyListener(this);
-				}
+			if (wordComplete == true) {
+				createNewWord();
+				wordComplete = false;
 			}
-			label.setText(underscores);
-			frame.pack();
-			frame.setVisible(true);
 		} else {
 			JOptionPane.showMessageDialog(null, "Try again");
 			run();
 		}
 	}
 
+	void createNewWord() {
+		wordComplete = false;
+		word = allWords.pop();
+		for (int j = 0; j < word.length(); j++) {
+			underscores += "_";
+		}
+		label.setText(underscores);
+		frame.pack();
+		System.out.println("hi");
+	}
+
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 		char typedIn = e.getKeyChar();
-		StringBuilder sb=new StringBuilder(label.getText());
+		StringBuilder sb = new StringBuilder(label.getText());
 		for (int i = 0; i < word.length(); i++) {
-			if (word.charAt(i)==typedIn) {
-sb.setCharAt(i, typedIn);
+			if (word.charAt(i) == typedIn) {
+				if(!typedInStack.contains(typedIn)) {
+					typedInStack.push(typedIn);
+				}
+				else {
+					sb.setCharAt(i, typedIn);
+					System.out.println(totalTyped);
+					frame.pack();
+					totalTyped++;
+				}
+			} else {
+				numberOfLives -= 1;
 			}
-			else(){
-			numberOfLives-=1;	
+			if (totalTyped == word.length()) {
+				wordComplete = true;
+				System.out.println("yee");
 			}
 		}
 		label.setText(sb.toString());
+		if (wordComplete == true) {
+			createNewWord();
+		}
 	}
 
 	@Override
